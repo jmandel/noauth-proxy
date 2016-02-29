3cd423a169ff2734af15e7d5c2dc2adc8e3a9750
Josh Mandel <jmandel@alum.mit.edu>
2016-02-29 00:52:21 -0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-docker run --name oauth-tester-debug \
+docker run --name noauth-proxy \
        -v $PWD/app:/app \
        -v /dev/shm:/dev/shm \
        -e APP_PORT=8000  \
        -e BASE_URL=myserver \
        -p 8000:8000 \
        --rm -it \
-       oauth-tester-debug
+       noauth-proxy
